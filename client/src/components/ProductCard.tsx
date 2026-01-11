23608a0d076d81f9744e776944469ddaee1a9d0b
Chetan <51225903+ChetanKawarkhe@users.noreply.github.com>
2026-01-12 00:32:30 +0530

    first commit

---
+++ client/src/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { ProductType } from '@/types'
+import React from 'react'
+
+const ProductCard = ({product}:{product:ProductType}) => { //take product from parent of productType 
+  return (
+    <div>ProductCard</div>
+  )
+}
+
+export default ProductCard
