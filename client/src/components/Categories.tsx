1305f769c55119fea7a7741661a4028c2f324dc9
Chetan <51225903+ChetanKawarkhe@users.noreply.github.com>
2026-01-12 14:29:51 +0530

    product list and redering completed

--- client/src/components/Categories.tsx
+++ client/src/components/Categories.tsx
@@ -11,6 +11,7 @@ import {
   Venus,
 } from "lucide-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Router } from 'next/router';
 
 const categories = [
   {
@@ -56,9 +57,42 @@ const categories = [
 ];
 
 const Categories = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter()
+  const pathname = usePathname()
+  const selectedCategory = searchParams.get("category");
+
+  console.log(selectedCategory)
+
+  const handleChange = (value:string | null)=>{
+    const params = new URLSearchParams(searchParams);
+    params.set("category", value || 'all')
+
+    router.push(`${pathname}?${params.toString()}`, {scroll: false})
+  }
+  //! Take existing URL params
+  //! Update only category
+  //! Keep others (sort, page, etc.)
+  //! Update URL without refresh
+
   return (
-    <div>Categories</div>
-  )
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid:cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-200 p-2 rounded-lg mb-4 text-sm">
+      {categories.map((category) => {
+        return (
+          <div
+            className={`flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md ${
+              category.slug === selectedCategory ? "bg-white" : "text-gray-500"
+            }`}
+            key={category.name}
+            onClick={()=>handleChange(category.slug)}
+          >
+            {category.icon}
+            {category.name}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Categories
